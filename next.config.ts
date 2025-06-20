7b768e574e7b78926e5337027c2b8752ca0c299f
ujjwalkuikel <ujjwalkuikel3@gmail.com>
2025-06-19 20:25:52 -0400

    Landing page

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  "rules": {
+  "react/no-unescaped-entities": "off",
+  "@typescript-eslint/no-unused-vars": "warn",
+  "react-hooks/exhaustive-deps": "warn"
+}
+
   /* config options here */
 };
 
